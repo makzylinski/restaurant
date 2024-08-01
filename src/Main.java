105afc28b79654b42181dbd14764778068b79e8a
Maks <makzylinski@gmail.com>
2024-08-01 18:17:24 +0200

    added method to pick side dish

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Burger burger = new Burger();
-        burger.assembleBurger();
-        burger.addToppings();
+//        Burger burger = new Burger();
+//        burger.assembleBurger();
+//        burger.addToppings();
+//
+//        System.out.println("Your bill: $" + burger.getPrice());
 
-        System.out.println("Your bill: $" + burger.getPrice());
+        SideItem sideItem = new SideItem();
+        sideItem.pickSideItem();
     }
 }
--- src/SideItem.java
+++ src/SideItem.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class SideItem {
     private String type;
-    private double price;
-    private String size;
+    private double price = 0.0;
+
+    public void getListOfItems() {
+        System.out.printf("%s%n".repeat(5),
+                "1) Fries - $1.99", "2) Coleslaw - $2.99", "3) Chili - $3.99", "4) Mashed Potatoes - $4.99", "5) Chips - $5.99");
+    }
+
+    public void pickSideItem() {
+        getListOfItems();
+        System.out.println();
+        Scanner s = new Scanner(System.in);
+        System.out.print("Pick your Side Item: ");
+        String selectedSideItem = s.nextLine();
+        switch (selectedSideItem) {
+            case "1" -> {
+                System.out.println("You picked Fries, price $1.99");
+                type = "small";
+                price += 1.99;
+            }
+            case "2" -> {
+                System.out.println("You picked Coleslaw, price $2.99");
+                type = "medium";
+                price += 2.99;
+            }
+            case "3" -> {
+                System.out.println("You picked Chili, price $3.99");
+                type = "large";
+                price += 3.99;
+            }
+            case "4" -> {
+                System.out.println("You picked Mashed Potatoes, price $4.99");
+                type = "large";
+                price += 4.99;
+            }
+            case "5" -> {
+                System.out.println("You picked Chips, price $5.99");
+                type = "large";
+                price += 5.99;
+            }
+            default -> System.out.println("Error occurred.");
+        }
+
+        System.out.println(price);
+    }
 }
